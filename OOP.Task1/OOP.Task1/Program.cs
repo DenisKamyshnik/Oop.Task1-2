48abbf1c826195525d6a8d27e869a25e95665f5f
Denis Kamyshnik <dkamyshnik66@gmail.com>
2021-02-26 18:08:43 +0200

    Version2

--- OOP.Task1/OOP.Task1/Program.cs
+++ OOP.Task1/OOP.Task1/Program.cs
@@ -5,9 +5,67 @@ namespace OOP.Task1
     public class Square
     {
 
-        public string a; // сторона квадрата
+       static public string a; // сторона квадрата
         public double S; // площадь квадрата
-        public Square() { a = Console.ReadLine(); } // вводим значение внутри конутруктора    
+        public Square()  // вводим значение внутри конутруктора  
+        {
+            
+            this.GetResult();
+
+        }
+        public void GetResult()
+        {
+            double A;
+            for (int i = 0; i <= 2; i++)
+            {
+
+                a = Console.ReadLine();
+                bool result = double.TryParse(a, out A);
+
+               if (result == true) {
+               A = Convert.ToDouble(a);
+                GetSquare();
+                    break;
+                    
+                }
+                else if (i<2)
+                {
+                    Console.WriteLine("Повторите попытку, система принимает только значения типа int и double, например: 5,25 или 10");
+                }
+                if (i == 2)
+                {
+                
+                GetRandomSquare();
+                   
+
+                }
+               
+            }        
+        }
+        public double GetRandomSide()
+        {
+           
+            Random rnd = new Random();
+            double a = (rnd.NextDouble() * (5 - 0.5)) + 0.5;
+            return a;
+        }
+
+       /* public double GetSide()
+        {
+            try
+            {
+                double A = Convert.ToDouble(a);
+                return A;
+            }
+            catch
+            {
+                double A = GetRandomSide();
+                return A;
+            }
+           
+            
+        }*/
+
         public void GetSquare() // процедура рассчета площади квадрата в случае успешного ввода
         {
             //S = a2
@@ -17,16 +75,17 @@ namespace OOP.Task1
             Console.WriteLine("Площадь квадрата =" + S);
         }
 
+
         public void GetRandomSquare() // процедура рассчета площади квадрата в случае, когда три попытки были неудачные 
         {
-            Random rnd = new Random();
-            double Size = (rnd.NextDouble() * (5 - 0.5)) + 0.5;
-            S = Size * Size;
+           double a = GetRandomSide();
+            S = a * a;
             S = Math.Round(S * 100.0) / 100.0;
-            Console.WriteLine("Сторона квадрата =" + Size);
+            Console.WriteLine("Сторона квадрата =" + a);
             Console.WriteLine("Площадь квадрата =" + S);
         }
     }
+   
 
     public class Circle
     {
@@ -34,15 +93,55 @@ namespace OOP.Task1
         public const double PI = 3.14;
         public string r; // радиус
         public double S; // площадь круга
+        
+        public Circle() // вводим значение внутри конутруктора 
+        {
+            this.GetResult();
+        }
+        public void GetResult()
+        {
+            double R;
+            for (int i = 0; i <= 2; i++)
+            {
 
-        public Circle() { r = Console.ReadLine(); } // вводим значение внутри конутруктора 
+                r = Console.ReadLine();
+                bool result = double.TryParse(r, out R);
+
+                if (result == true)
+                {
+                    R = Convert.ToDouble(r);
+                    GetSquareCircle();
+                    break;
+
+                }
+                else if (i < 2)
+                {
+                    Console.WriteLine("Повторите попытку, система принимает только значения типа int и double, например: 5,25 или 10");
+                }
+                if (i == 2)
+                {
+
+                    GetRandomSquareCircle();
+
+
+                }
+
+            }
+        }
+
         public void GetSquareCircle() // процедура рассчета площади круга в случае успешного ввода
         {
             double R = Convert.ToDouble(r);
             S = (R * R) * PI;
             S = Math.Round(S * 100.0) / 100.0;
             Console.WriteLine("Площадь круга =" + S);
+            
         }
+      /*  public double GetDiameter()
+        {
+            double R = Convert.ToDouble(r);
+            return R * 2;
+        }*/
 
         public void GetRandomSquareCircle() // процедура рассчета площади круга в случае, когда три попытки были неудачные 
         {
@@ -52,63 +151,119 @@ namespace OOP.Task1
             S = Math.Round(S * 100.0) / 100.0;
             Console.WriteLine("Радиус =" + R);
             Console.WriteLine("Площадь круга =" + S);
+
         }
 
-        public class Program
+    }
+
+    public class Program
+    {
+        
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
 
-                Console.WriteLine("Вычисляем площадь квадрата, введите сторону квадрата");
+            Console.WriteLine("Вычисляем площадь квадрата, введите сторону квадрата");
 
-                for (int i = 0; i <= 1; i++)
-                {
-                    try
-                    {
-                        Square test = new Square();
-                        test.GetSquare();
+            Square test = new Square();
+            string A = Square.a;
 
-                        break;
-                    }
-                    catch
-                    {
-                        if (i <= 1)
-                            Console.WriteLine("Повторите попытку, система принимает только значения типа int и double, например: 5,25 или 10");
-                        if (i == 1)
-                        {
-                            Square test = new Square();
-                            test.GetRandomSquare();
-                        
-                        }
-                    }
 
-                }
+            Console.WriteLine(A);
 
-                Console.WriteLine("Вычисляем площадь круга, введите радиус");
+         
+            Console.WriteLine("Вычисляем площадь круга, введите радиус");
 
-                for (int i = 0; i <= 1; i++)
-                {
-                    try
-                    {
-                        Circle test = new Circle();
-                        test.GetSquareCircle();
-
-                        break;
-                    }
-                    catch
-                    {
-                        if (i <= 1)
-                            Console.WriteLine("Повторите попытку, система принимает только значения типа int и double, например: 5,25 или 10");
-                        if (i == 1)
-                        {
-                            Circle test = new Circle();
-                            test.GetRandomSquareCircle();
-
-                        }
-                    }
-                }
-            }
+            Circle test1 = new Circle();
         }
     }
 }
+
+
+            //test.GetSide();
+            //   Console.WriteLine("Сторона" + test.GetSide());
+
+
+            //Console.WriteLine(test.GetSide());
+            /*  double d;
+              double side;
+              string a;
+
+              Console.WriteLine("Вычисляем площадь квадрата, введите сторону квадрата");
+
+              Square test = new Square();
+
+
+              for (int i = 0; i <= 1; i++)
+              {
+                  try
+                  {
+
+                      test.GetSquare();
+
+
+
+                      break;
+                  }
+                  catch
+                  {
+                      if (i <= 1)
+                          Console.WriteLine("Повторите попытку, система принимает только значения типа int и double, например: 5,25 или 10");
+                      if (i == 1)
+                      {
+
+                          test.GetRandomSquare();
+
+                      }
+                  }
+
+              }
+
+            /*  Console.WriteLine("Вычисляем площадь круга, введите радиус");
+
+
+              for (int i = 0; i <= 1; i++)
+              {
+                  try
+                  {
+                      Circle test = new Circle();
+                      test.GetSquareCircle();
+
+
+                      break;
+                  }
+                  catch
+                  {
+                      if (i <= 1)
+                          Console.WriteLine("Повторите попытку, система принимает только значения типа int и double, например: 5,25 или 10");
+                      if (i == 1)
+                      {
+                          Circle test = new Circle();
+                          test.GetRandomSquareCircle();
+
+                      }
+                  }
+
+
+              }
+              Console.WriteLine("Определяем поместится ли круг в квадрат:");
+
+              d = test.GetDiameter();
+              Console.WriteLine("Диаметр:" + d);
+
+              side = test.GetSide();
+              Console.WriteLine("Сторона квадрата:" + side);
+
+
+              /*  if (d < side)
+                {
+
+                }*/
+
+            //Circle test1 = new Circle();
+            //Square test2 = new Square();
+            // test1.GetDiameter();
+            //Console.WriteLine(test.GetDiameter());
+
+
+
 
