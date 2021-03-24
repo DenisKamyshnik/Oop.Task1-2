ff6a0edb81f3611aa8179d239eee4ee10a3ed5ea
Denis Kamyshnik <dkamyshnik66@gmail.com>
2021-03-24 13:56:58 +0200

    Final version

--- OOP.Task1/OOP.Task1/Square.cs
+++ OOP.Task1/OOP.Task1/Square.cs
@@ -5,23 +5,13 @@ namespace OOP.Task1
     public class Square
     {
         public double A { get; set; }          
-      //  private double S { get; set; } 
-
         public double Deagonal => InputAndValidation.GetRoundedValue(A * Math.Sqrt(2));
-
         public double S => InputAndValidation.GetRoundedValue(A * A);
 
         public Square()  
         {
             A = InputAndValidation.GetInput();
-            //SetSquareOfSquare();
         }
-
-   /*     private void SetSquareOfSquare() 
-        {
-            S = A * A;
-            S = Math.Round(S * 100.0) / 100.0;       
-        }*/
 
         public void PrintSquareSide()
         {
