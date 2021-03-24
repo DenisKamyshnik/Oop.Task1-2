d735c29e140cce1b0d66df6414afc588c53894b2
Denis Kamyshnik <dkamyshnik66@gmail.com>
2021-03-24 15:12:01 +0200

    Final version

---
+++ OOP.Task1-2/OOP.Task1-2/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP.Task1_2
+{
+    public class Circle
+    {
+        private const double PI = 3.14;
+        public double R { get; set; }
+        public double Diametr => InputAndValidation.GetRoundedValue(R * 2);
+        public double S => InputAndValidation.GetRoundedValue((R * R) * PI);
+
+        public Circle()
+        {
+            R = InputAndValidation.GetInput();
+        }
+
+        public void PrintRadius()
+        {
+            Console.WriteLine("Радиус = " + R);
+        }
+
+        public void PrintCircleArea()
+        {
+            Console.WriteLine("Площадь круга = " + S);
+        }
+
+    }
+}
